ba97dcc2a9d879c4abe51de41788eebf2ef12b1b
xj <xj@kanjisod.kicks-ass.net>
2011-04-24 12:47:36 +0900

    updating the header and adding comments

--- image_processing.h
+++ image_processing.h
@@ -1,6 +1,7 @@
 #include <Accelerate/Accelerate.h>
 #include <ApplicationServices/ApplicationServices.h>
 
+/* Data structure for each image */
 typedef struct {
 	int image_rot;
 	int image_w;
@@ -12,20 +13,22 @@ typedef struct {
 
 typedef image_properties *img_prop;
 
-// This function takes the command line arguments and 1 file name
+/* This function takes the command line arguments and 1 file name */
 void process_1_image (args cli_flags, char *file_name);
 
-/****************************/
-/****** Get EXIF data *******/
-/****************************/
+/* Get EXIF data */
 int get_exif_rot (CGDataProviderRef image_source);
 
+/* Flip the image 180 */
 void flip_image (vImage_Buffer *vImage_source, img_prop image_prperties, args cli_flags);
 
+/* Rotate the image by the EXIF rotation tag specified amount */
 void rotate_image (vImage_Buffer *vImage_source, img_prop image_prperties, args cli_flags);
 
+/* Resize the image to the required size or by default of 800px long side */
 void resize_image (vImage_Buffer *vImage_source, img_prop image_prperties, args cli_flags);
 
+/* Save the image to disk */
 void save_image (vImage_Buffer *vImage_source, img_prop image_prperties, float compression, char *file_name);
 
 double calcImageLongSide (int shortside, int sShort, int sLong);
@@ -35,6 +38,8 @@ double calcImageShortSide (int longside, int sShort, int sLong);
 void print_vBuff (vImage_Buffer *buffer);
 
 void setupBuffer (vImage_Buffer *buffer, int height, int width, int rowBytes);
+
+void* convert_space (const void *source_data_ptr, int width, int height);
 
 void null_ip (img_prop image_prperties);
 
